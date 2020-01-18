3653096dbd23791f7dfd489c3bda7391dde3218e
Haydn Ewers <27211+haydn@users.noreply.github.com>
2020-01-18 16:36:14 +1100

    Update the example file
    Fixes #26

--- src/Editor.js
+++ src/Editor.js
@@ -96,29 +96,118 @@ const Editor = () => {
     doc: {
       layers: [
         {
-          id: "1",
+          id: "-LyqdJBMdrVihqnJOOo8",
+          name: "Input",
+          component: "Input",
+          x1: 100,
+          y1: 666,
+          x2: 500,
+          y2: 722,
+          options: { label: "Email", value: "" }
+        },
+        {
+          id: "-LyqdsUuufs_UM05V3My",
+          name: "Button",
+          component: "Button",
+          x1: 417.78125,
+          y1: 732,
+          x2: 500,
+          y2: 768,
+          options: { label: "Subscribe" }
+        },
+        {
+          id: "-LyqduQ0G84esGuBLiC4",
+          name: "Image",
+          component: "Image",
+          x1: 100,
+          y1: 156,
+          x2: 340,
+          y2: 336
+        },
+        {
+          id: "-Lyr1QcLkjIhPrr7ojKV",
           name: "Heading 1",
           component: "Heading 1",
-          x1: 50,
+          x1: 100,
           y1: 100,
-          x2: 250,
-          y2: 150,
+          x2: 392.75,
+          y2: 136,
+          options: { text: "Example Website" }
+        },
+        {
+          id: "-Lyr1U4LM637a9qwzp8I",
+          name: "Heading 2",
+          component: "Heading 2",
+          x1: 350,
+          y1: 156,
+          x2: 498.34375,
+          y2: 180,
+          options: { text: "Lorem ipsum" }
+        },
+        {
+          id: "-Lyr1aKzs3cjCk5yyD1R",
+          name: "Paragraph",
+          component: "Paragraph",
+          x1: 350,
+          y1: 190,
+          x2: 750,
+          y2: 310,
           options: {
-            text: "Hello World"
+            text:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
           }
         },
         {
-          id: "2",
+          id: "-Lyr2YAEGxGezgs2k5sV",
+          name: "Image",
+          component: "Image",
+          x1: 510,
+          y1: 356,
+          x2: 750,
+          y2: 536
+        },
+        {
+          id: "-Lyr2n5QEWuXLOBwMjlt",
+          name: "Heading 2",
+          component: "Heading 2",
+          x1: 100,
+          y1: 356,
+          x2: 248.34375,
+          y2: 380,
+          options: { text: "Lorem ipsum" }
+        },
+        {
+          id: "-Lyr2pytYlfINHOHjB37",
           name: "Paragraph",
           component: "Paragraph",
           x1: 100,
-          y1: 150,
-          x2: 350,
-          y2: 300,
+          y1: 390,
+          x2: 500,
+          y2: 510,
           options: {
             text:
-              "Now is the time for all good men to come to the aid of their party."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
           }
+        },
+        {
+          id: "-Lyr2zBoU64dhef9SyDI",
+          name: "Heading 2",
+          component: "Heading 2",
+          x1: 100,
+          y1: 632,
+          x2: 248.34375,
+          y2: 656,
+          options: { text: "Lorem ipsum" }
+        },
+        {
+          id: "-Lyr7tPI3CN8t_0-x0X0",
+          name: "Heading 1",
+          component: "Heading 1",
+          x1: 100,
+          y1: 576,
+          x2: 340.640625,
+          y2: 612,
+          options: { text: "Example Form" }
         }
       ]
     },
