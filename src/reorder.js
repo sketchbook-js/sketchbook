0c0a820660501a0e4fada556e3ceca90c628d885
Michael <tranmichael97@gmail.com>
2020-02-25 10:11:11 +1100

    Fix documentation

--- src/reorder.js
+++ src/reorder.js
@@ -6,8 +6,9 @@ import { set } from "set-fns";
   Params:
     list: list of elements to reorder.
     reorderIndexes: either a Number or an Array of Numbers. These Numbers are the indexes of the elements we want to reorder in the list argument.
-      If reorderIndexes is an array, the element in the list argument at index `reorderIndexes[0]` will be placed at index `endIndex` and the elements in the list argument represented by the rest of the indexes in reorderIndexes will be sorted in ascending order.
-    endIndex: the index at which we want to put the element at index `reorderIndexes[0] || reorderIndexes` after having removed the element at index `reorderIndexes[0] || reorderIndexes` in the list argument.
+      If reorderIndexes is a Number, reorder(["a", "b", "c", "d"], 1, 3) -> ["a", "c", "d", "b"]
+      If reorderIndexes is an Array, the element at index reorderIndexes[0] will be reordered as above. The elements in `list` at the indexes in the rest of the array will be sorted by their natural index and placed to the right of the new position of the element at index `endIndex`
+    endIndex: the index in `list` at which we want to put the element at index `reorderIndexes[0] || reorderIndexes` after having removed the element at index `reorderIndexes[0] || reorderIndexes` in `list`.
 */
 const reorder = (list, reorderIndexes, endIndex) => {
   const leadIndex = Array.isArray(reorderIndexes)
