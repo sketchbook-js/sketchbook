81d5ea4285fa07fe58065d7de57c5e40ebc9fc43
Michael <tranmichael97@gmail.com>
2020-08-21 06:35:13 +1000

    feat(Editor): Render renderers in editor app

--- src/Editor.js
+++ src/Editor.js
@@ -1481,13 +1481,15 @@ const Editor = ({ config }) => {
               ) : (
                 (
                   <Fragment>
-                    {["Root", ...path].map(pathKey => {
+                    {/* RENDER PATH BAR */}
+                    {["Root", ...path].map(pathItem => {
                       return (
                         <button
+                          className="path"
                           onClick={() => {
                             setPath(currPath => {
                               const keyIndex = currPath.findIndex(
-                                path => path === pathKey
+                                path => path === pathItem
                               );
                               return currPath.length === 1
                                 ? [] // Render the root level
@@ -1495,10 +1497,17 @@ const Editor = ({ config }) => {
                             });
                           }}
                         >
-                          {pathKey}
+                          {pathItem}
+                          {">"}
+                          <style jsx>{`
+                            .path:hover {
+                              color: "pink";
+                            }
+                          `}</style>
                         </button>
                       );
                     })}
+                    {/* BACK BUTTON */}
                     <button
                       onClick={() => {
                         setPath(path => path.slice(0, path.length - 1));
@@ -1506,49 +1515,56 @@ const Editor = ({ config }) => {
                     >
                       Back
                     </button>
-                    <RecordRenderer
-                      fields={
-                        config.components.find(
-                          ({ type }) =>
-                            doc.layers.find(({ id }) => id === 101010)
-                              .component === type
-                        ).options
-                      }
-                      values={
-                        doc.layers.find(({ id }) => id === 101010).options
-                      }
-                      path={path}
-                      depth={depth}
-                      onNavigate={newPaths =>
-                        setPath(path => [...path, ...newPaths])
-                      }
-                      onChange={(index, key, value) =>
-                        // TODO: This doesn't work.
-                        // Pass in an index when wanting to modify an array element, otherwise pass in null.
-                        setState(current => ({
-                          ...current,
-                          doc: {
-                            layers: current.doc.layers.map(layer =>
-                              layer.id === [...selection][0]
-                                ? {
-                                    ...layer,
-                                    options: {
-                                      ...layer.options,
-                                      [key]:
-                                        index === null
-                                          ? value
-                                          : layer.options.key.map(
-                                              (keyValue, i) =>
-                                                i === index ? value : keyValue
-                                            )
+                    {config.components.find(
+                      ({ type }) =>
+                        doc.layers.find(({ id }) => id === 101010).component ===
+                        type
+                    ).options ? (
+                      <RecordRenderer
+                        fields={
+                          config.components.find(
+                            ({ type }) =>
+                              doc.layers.find(({ id }) => id === 101010)
+                                .component === type
+                          ).options
+                        }
+                        values={
+                          // TODO: id === selected layer instead of 101010
+                          doc.layers.find(({ id }) => id === 101010).options
+                        }
+                        path={path}
+                        depth={depth}
+                        onNavigate={newPaths =>
+                          setPath(path => [...path, ...newPaths])
+                        }
+                        onChange={(index, key, value) =>
+                          // TODO: This doesn't work because it requires you to update the state more than 1 layer. IT also has to be able to handle key value objects and arrays.
+                          // Pass in an index when wanting to modify an array element, otherwise pass in null.
+                          setState(current => ({
+                            ...current,
+                            doc: {
+                              layers: current.doc.layers.map(layer =>
+                                layer.id === [...selection][0]
+                                  ? {
+                                      ...layer,
+                                      options: {
+                                        ...layer.options,
+                                        [key]:
+                                          index === null
+                                            ? value
+                                            : layer.options.key.map(
+                                                (keyValue, i) =>
+                                                  i === index ? value : keyValue
+                                              )
+                                      }
                                     }
-                                  }
-                                : layer
-                            )
-                          }
-                        }))
-                      }
-                    />
+                                  : layer
+                              )
+                            }
+                          }))
+                        }
+                      />
+                    ) : null}
                   </Fragment>
                 ) ?? (
                   <div
