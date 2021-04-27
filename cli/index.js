239aa601ed47e3eed300da661e81b7512fdae95d
Mxchaeltrxn <tranmichael97@gmail.com>
2021-04-27 20:46:47 +1000

    feat: Create api for editor to access design file

--- cli/index.js
+++ cli/index.js
@@ -79,18 +79,43 @@ switch (command) {
 
     const app = express();
 
+    app.set("json spaces", 2);
+    app.use(express.json());
+    app.use(express.urlencoded());
+
     app.get("/env.js", (req, res) => {
       res.type("text/javascript");
       res.send("window.SKETCHBOOK_MODE = 'interactive';");
     });
     app.get("/config.js", (req, res) => res.sendFile(configFile));
-    app.get("/designs.json", (req, res) =>
-      res.json(
-        glob.sync("**/*.json", {
-          cwd: path.join(__dirname, "../build/designs"),
-        }),
-      ),
+    app.get(
+      "/designs.json",
+      (req, res) => res.json([args["--file"]]),
+      // TODO: Code to get multiple design files from the design directory.
+      // res.json(
+      //   glob.sync("**/*.json", {
+      //     cwd: path.join(__dirname, "../build/designs"),
+      //   }),
+      // ),
     );
+    app.get("/design", (req, res) => {
+      res.sendFile(designFilePath);
+    });
+    app.post("/design", (req, res) => {
+      try {
+        fs.writeFileSync(designFilePath, JSON.stringify(req.body.doc, null, 2));
+        return res.json({
+          statusCode: 200,
+          message: `Design file successfully saved to ${designFilePath}.`,
+        });
+      } catch (err) {
+        console.error(err);
+        return {
+          statusCode: 500,
+          message: `Design file unable to be saved to ${designFilePath}. Please try again.`,
+        };
+      }
+    });
     app.use("/canvas", express.static(path.join(__dirname, "../build/canvas")));
     app.use("/editor", express.static(path.join(__dirname, "../build/editor")));
     app.use("/designs", express.static(designsDir));
