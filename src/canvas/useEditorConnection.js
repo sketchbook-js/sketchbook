2ef15bb162c9a950f9cb90a49f596b5c1a2b248d
Mxchaeltrxn <tranmichael97@gmail.com>
2021-09-11 12:02:43 +1000

    Add id to request and responses

--- src/canvas/useEditorConnection.js
+++ src/canvas/useEditorConnection.js
@@ -1,38 +1,58 @@
 import { useState, useEffect } from "react";
+import pushID from "../pushID";
 
 const useEditorConnection = (editorWindow, canvasWindow) => {
   const [layersToRender, setLayersToRender] = useState([]);
   const [layersToMeasure, setLayersToMeasure] = useState([]);
+  const [sketchbookRequestMeasureId, setSketchbookRequestMeasureId] = useState(
+    undefined
+  );
+
   useEffect(() => {
     const receiveMessage = event => {
       switch (event.data.type) {
         case "sketchbook_request:ping": {
-          editorWindow.postMessage(
-            {
-              type: "sketchbook_response:acknowledge",
-              status: "ready"
-            },
-            "*"
-          );
+          postSketchbookMessageAcknowledge();
           break;
         }
         case "sketchbook_request:render": {
+          postSketchbookMessageAcknowledge();
           setLayersToRender(event.data.layers);
           break;
         }
         case "sketchbook_request:measure": {
+          setSketchbookRequestMeasureId(event.data.id);
           setLayersToMeasure(current => [...current, event.data.layer]);
           break;
         }
         default:
           break;
       }
+
+      function postSketchbookMessageAcknowledge() {
+        editorWindow.postMessage(
+          {
+            type: "sketchbook_response:acknowledge",
+            id: pushID(),
+            request: event.data.id,
+            status: "ready"
+          },
+          "*"
+        );
+      }
     };
     canvasWindow.addEventListener("message", receiveMessage);
     return () => {
       canvasWindow.removeEventListener("message", receiveMessage);
     };
-  }, [setLayersToRender, setLayersToMeasure, canvasWindow, editorWindow]);
+  }, [
+    setLayersToRender,
+    setLayersToMeasure,
+    canvasWindow,
+    editorWindow,
+    sketchbookRequestMeasureId
+  ]);
+
   return {
     layersToRender,
     layersToMeasure,
@@ -41,6 +61,7 @@ const useEditorConnection = (editorWindow, canvasWindow) => {
         {
           type: "sketchbook_response:measure",
           id,
+          request: sketchbookRequestMeasureId,
           width,
           height
         },
